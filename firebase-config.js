8a111ea2ef58a253d6b4130547dd1e800a2f97f7
Amy-Tung <abc601285@gmail.com>
2026-08-16 04:34:23 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.DINNER_WHEEL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDYBsLduV9Du78PaN6tXDsX09MN-7z_4hA",
+  authDomain: "dinner-wheel-61c2a.firebaseapp.com",
+  projectId: "dinner-wheel-61c2a",
+  storageBucket: "dinner-wheel-61c2a.firebasestorage.app",
+  messagingSenderId: "624209509587",
+  appId: "1:624209509587:web:efafd7b68cc504c9de6826"
+};
